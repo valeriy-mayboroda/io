6310d77893b1e27bc51f17b6b107bc1a63a87dd6
Valeriy Mayboroda <valero1@mail.ru>
2016-10-07 22:15:45 +0300

    make conditions if-else by using boolean variables add methods more information

--- src/SAME.java
+++ src/SAME.java
@@ -29,6 +29,9 @@ import java.util.List;
 public class SAME
 {
     public static List<LineItem> lines = new ArrayList<LineItem>();
+    public static List<String> list1 = new ArrayList<String>();//Список строк из файла №1
+    public static List<String> list2 = new ArrayList<String>();//Список строк из файла №2
+    public static boolean operation = true;//operation = true - pазрешить ADDED или REMOVED
     public static void main(String[] args) throws IOException
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -36,62 +39,74 @@ public class SAME
         String filename2 = reader.readLine();
         reader.close();
 
-        ArrayList<String> list1 = makelist(filename1);
-        ArrayList<String> list2 = makelist(filename2);
-        String line1;
-        String line2;
-        Type type;
+        list1 = makelist(filename1);
+        list2 = makelist(filename2);
 
-        boolean operation = true;
+        String line1 = "";
+        String line2 = "";
+
         for (int i = 0; i < list1.size() || i < list2.size();)
         {
-            if (list1.isEmpty() && operation)
-            {
-                type = Type.ADDED;
-                lines.add(new LineItem(type, list2.get(i)));
-                list2.remove(i);
-                operation = false;
+            line1 = list1.isEmpty()? null   : list1.get(i);
+            line2 = list2.isEmpty()? ""     : list2.get(i);
+            boolean listsnotempty = !list1.isEmpty() && !list2.isEmpty();
+
+            boolean permitREMOVED = operation
+                    && (list2.isEmpty()
+                    || (listsnotempty && ((i + 1) < list1.size() && list1.get(i + 1).equals(line2))));
+            boolean permitADD     = operation
+                    && (list1.isEmpty()
+                    || (listsnotempty && ((i + 1) < list2.size() && list2.get(i + 1).equals(line1))));
+            boolean permitSAME    = listsnotempty && line1.equals(line2);
+
+            if (permitREMOVED) {
+                makeREMOVED(i);
             }
-            else if (list2.isEmpty() && operation)
-            {
-                type = Type.REMOVED;
-                lines.add(new LineItem(type, list1.get(i)));
-                list1.remove(i);
-                operation = false;
+            else if (permitADD) {
+                makeADDED(i);
             }
-            else if (!list1.isEmpty() && !list2.isEmpty())
-            {
-                line1 = list1.get(i);
-                line2 = list2.get(i);
-                if (line1.equals(line2))
-                {
-                    type = Type.SAME;
-                    lines.add(new LineItem(type, line1));
-                    list1.remove(i);
-                    list2.remove(i);
-                    operation = true;
-                }
-                else if ((i + 1) < list1.size() && operation && list1.get(i + 1).equals(line2))
-                {
-                    type = Type.REMOVED;
-                    lines.add(new LineItem(type, line1));
-                    list1.remove(i);
-                    operation = false;
-                }
-                else if ((i + 1) < list2.size() && operation && list2.get(i + 1).equals(line1))
-                {
-                    type = Type.ADDED;
-                    lines.add(new LineItem(type, line2));
-                    list2.remove(i);
-                    operation = false;
-                }
-                else
-                {
-                    System.out.println("BREAK");
-                    break;
-                }
+            else if (permitSAME) {
+                makeSAME(i);
+            }
+            else {
+                System.out.println("Данные в файлах не корректны");
+                break;
             }
         }
+        for (LineItem l : lines) {
+            System.out.println(l.type + " " + l.line);
+        }
+    }
+    public static ArrayList<String> makelist(String filename) throws IOException
+    {
+        ArrayList<String> list = new ArrayList<String>();
+        BufferedReader filereader = new BufferedReader(new FileReader(filename));
+        String line;
+        while((line = filereader.readLine()) != null)
+        {
+            list.add(line);
+        }
+        filereader.close();
+        return list;
+    }
+    public static void makeSAME(int j)
+    {
+        lines.add(new LineItem(Type.SAME, list1.get(j)));
+        list1.remove(j);
+        list2.remove(j);
+        operation = true;
+    }
+    public static void makeADDED(int j)
+    {
+        lines.add(new LineItem(Type.ADDED, list2.get(j)));
+        list2.remove(j);
+        operation = false;
+    }
+    public static void makeREMOVED(int j)
+    {
+        lines.add(new LineItem(Type.REMOVED, list1.get(j)));
+        list1.remove(j);
+        operation = false;
     }
     public static enum Type
     {
@@ -108,17 +123,5 @@ public class SAME
             this.type = type;
             this.line = line;
         }
-    }
-    public static ArrayList<String> makelist(String filename) throws IOException
-    {
-        ArrayList<String> list = new ArrayList<String>();
-        BufferedReader filereader = new BufferedReader(new FileReader(filename));
-        String line;
-        while((line = filereader.readLine()) != null)
-        {
-            list.add(line);
-        }
-        filereader.close();
-        return list;
     }
 }
